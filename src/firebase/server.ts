7c39d09f2674d2e225f608013a2398f268001636
Suresh <sbandaraofficial@gmail.com>
2025-11-25 21:51:45 +0000

    Try fixing this error: `Runtime Error: Error: 2 UNKNOWN: Getting metadat

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -5,21 +5,21 @@ import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getStorage } from 'firebase-admin/storage';
 
-let app: App;
-if (getApps().length === 0) {
-  app = initializeApp();
-} else {
-  app = getApp();
-}
-
-const firestore = getFirestore(app);
-const auth = getAuth(app);
-const storage = getStorage(app).bucket(process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET);
-
 /**
  * Initializes and returns Firebase Admin SDK services.
- * This function ensures that the app is initialized only once.
+ * This function ensures that the app is initialized only once per instance.
  */
 export async function initializeFirebase() {
+  let app: App;
+  if (getApps().length === 0) {
+    app = initializeApp();
+  } else {
+    app = getApp();
+  }
+
+  const firestore = getFirestore(app);
+  const auth = getAuth(app);
+  const storage = getStorage(app).bucket(process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET);
+
   return { app, auth, firestore, storage };
 }
