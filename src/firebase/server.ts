e96871e69094656806e12b9c5abfdd26005de2c8
Suresh <sbandaraofficial@gmail.com>
2025-11-25 21:01:06 +0000

    fix

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -1,22 +1,20 @@
-import { initializeApp, getApp, getApps, FirebaseApp } from 'firebase-admin/app';
+import { initializeApp, getApp, getApps, type App } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getStorage } from 'firebase-admin/storage';
 
-const app = getApps().length
-  ? getApp()
-  : initializeApp();
+let app: App;
 
-
-function getSdks(app: FirebaseApp) {
-  return {
-    app,
-    auth: getAuth(app),
-    firestore: getFirestore(app),
-    storage: getStorage(app).bucket(process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET)
-  };
+if (!getApps().length) {
+  app = initializeApp();
+} else {
+  app = getApp();
 }
 
+const firestore = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app).bucket(process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET);
+
 export function initializeFirebase() {
-    return getSdks(app);
+  return { app, auth, firestore, storage };
 }
